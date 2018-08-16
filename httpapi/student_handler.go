ff0fa9009a31848c83bf80794b0d6d9896eefbd9
Kory Prince <korylprince@gmail.com>
2018-08-15 22:46:58 -0500

    reformat student list

--- httpapi/student_handler.go
+++ httpapi/student_handler.go
@@ -9,12 +9,25 @@ import (
 
 //GET /students
 func handleReadStudentList(w http.ResponseWriter, r *http.Request) *handlerResponse {
+	type student struct {
+		Name    string `json:"name"`
+		OtherID string `json:"other_id"`
+		Grade   int    `json:"grade"`
+	}
+
+	type response []*student
+
 	students, err := api.GetStudentList(r.Context())
 	if resp := checkAPIError(err); resp != nil {
 		return resp
 	}
 
-	return &handlerResponse{Code: http.StatusOK, Body: ReadStudentListResponse{Students: students}}
+	var list response
+	for _, s := range students {
+		list = append(list, &student{Name: s.Name(), OtherID: s.OtherID, Grade: s.Grade})
+	}
+
+	return &handlerResponse{Code: http.StatusOK, Body: list}
 }
 
 //GET /students/:otherID/status
